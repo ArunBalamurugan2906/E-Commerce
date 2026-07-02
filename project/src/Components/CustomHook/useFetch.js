25de956c64913a1184dad29836e0ad94571e73fd
Arun Balamurugan <arunnandy36@gmail.com>
2026-07-02 23:10:00 +0530

    Created E-Commerce Project

--- project/src/Components/CustomHook/useFetch.js
+++ project/src/Components/CustomHook/useFetch.js
@@ -1,37 +1,3 @@
-// import { useEffect, useState } from "react";
-
-// function useFetch(url){
-//     let [products,setProducts] = useState([]);
-//     let [error,setError] = useState("");
-//     let [isLoading,setLoading] = useState(true)
-
-//     useEffect(() => {
-//         let fetchApi =  async() => {
-//           try{
-//           let response = await fetch(url);
-//           if(response.ok){
-//              let data = await response.json()
-//              setProducts(data)
-//           }else{
-//             throw new Error("Data Not found")
-//           }
-//           }catch{(error) => {
-//             setError(error.message)
-//           }
-//           }finally{
-//             setLoading(false)
-//           }
-
-//         }
-//         fetchApi()
-
-//     },[])
-//     return {products,error,isLoading}
-
-// }
-
-// export default useFetch;
-
 import { useEffect, useState } from "react";
 import axois from "axios";
 
